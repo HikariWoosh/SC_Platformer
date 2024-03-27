f4253756c3345e609d68960284cebd1c4f520e7c
HikariWoosh <wps21vfu@uea.ac.uk>
2024-03-27 22:22:22 +0700

    Added settings

--- Assets/Scipts/MainMenu.cs
+++ Assets/Scipts/MainMenu.cs
@@ -2,9 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    public AudioMixer myMix;
+
+    [SerializeField]
+    public Slider vol_Slider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetVolume();
+        }
+
+        if (PlayerPrefs.HasKey("qualityLevel"))
+        {
+            LoadQuality();
+        }
+    }
 
     public void PlayGame()
     {
@@ -14,6 +38,30 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume()
+    {
+        float volume = vol_Slider.value;
+        myMix.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    private void LoadVolume() 
+    {
+        vol_Slider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    public void QualitySet(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("qualityLevel", qualityIndex);
+
+    }
+
+    private void LoadQuality()
+    {
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityLevel"));
     }
 
 }
